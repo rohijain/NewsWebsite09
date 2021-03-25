36c0108b863289465b6af5210c743c1105ba67f8
Anu Jain <62193600+Anu-Jain827@users.noreply.github.com>
2021-03-25 19:34:21 +0100

    adding post news call

--- js/ContentController.js
+++ js/ContentController.js
@@ -1,6 +1,7 @@
 function getNewsItemsForDate() {
     console.log(_config.api.invokeUrl);
 
+
 	fetch(_config.api.invokeUrl + '/getnews' + "?news_date=09-01-1993")
 	.then(response => response.json())
 	.then(data => console.log(data));
@@ -10,3 +11,24 @@ function getNewsItemsForDate() {
 function completeGetRequest() {
     console.log("completed");
 }
+
+ function createNewsItem() {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/createnewsitem',
+            data: JSON.stringify({
+                NewsItem: {
+                    news_date: "09-09-1993",
+                    news_title: "news title",
+					news_body: "some news from somewhere"
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error CREATING ITEM: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+            }
+        });
+    }
+
